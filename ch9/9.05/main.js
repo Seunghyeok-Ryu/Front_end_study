77fc16976b846fcb23db46f5f229517a3bbaf0bc
SeunghyeokRyu <harrisonryu@naver.com>
2022-01-20 17:35:48 +0900

    22/01/20/9.05

---
+++ ch9/9.05/main.js
@@ -0,0 +1,20 @@
+/*
+let isShow = true;
+let checked = false;
+
+if(isShow){
+  console.log('Show!'); // Show!
+}
+
+if(checked){
+  console.log('Chceked!');
+}*/
+
+let isShow = false;
+
+if (isShow){
+  console.log('Show!!');
+}else{
+  console.log('Hide?');
+}
+
