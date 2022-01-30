4500e993cd4ad4eec9a9cd0b923ed96deb9716be
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-01-30 20:34:58 +0900

    22/01/30/03_02_04

--- part3/js_study_1/main.js
+++ part3/js_study_1/main.js
@@ -9,3 +9,17 @@ console.log(getType(false));
 console.log(getType(null));
 console.log(getType({}));
 console.log(getType([]));
+
+// 산술 연산(arithmetic operator)
+console.log(1 + 2);
+console.log(5 - 7);
+console.log(3 * 4);
+console.log(10 / 2);
+console.log(7 % 5);     // 나머지 연산자(%)
+
+// 할당 연산자(assignment operator), const(재할당 불가)
+
+let a = 2;
+// a = a + 1;
+a += 2;     // +대신 다른 산술연산자(/,*,%,-) 모두 사용 가능
+console.log(a);
