0df48f174623bfbaaef232f295ff0dfb2d697141
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-01-31 17:50:13 +0900

    22/01/31/03_02_08

--- part3/js_study_1/main.js
+++ part3/js_study_1/main.js
@@ -92,3 +92,21 @@ switch (i) {
   default:
     console.log('rest!!')
 }
+
+
+// 반복문 (For statement)
+// for (시작조건; 종료조건; 변화조건) {}
+
+const ulEl = document.querySelector('ul');
+console.log(ulEl);
+
+for (let j=0 ; j<10; j+=1) {
+  const li = document.createElement('li')
+  li.textContent = `list-${j + 1}`
+  if ((j+1) % 2 === 0) {
+    li.addEventListener('click', function () {
+      console.log(li.textContent)
+    })
+  }
+  ulEl.appendChild(li)
+}
