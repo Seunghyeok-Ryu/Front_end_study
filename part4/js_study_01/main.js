205f355779370af45df65e4aeb3cbe321cd697d6
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-02-01 17:58:41 +0900

    22/02/01/04_01_04

--- part4/js_study_01/main.js
+++ part4/js_study_01/main.js
@@ -57,3 +57,51 @@ const b = fruits.map((fruit, index) => ({
 }))
 console.log(b)
 
+// .filter()
+const c = numbers.map(number => number < 3)
+console.log(c)
+
+const d = numbers.filter(number => number < 3)
+console.log(d)
+
+// .find() .findIndex()
+
+const e = fruits.find(fruit => /^B/.test(fruit)     // 정규표현식 B로 시작하는 단어 true 반환 함수 종료
+)
+console.log(e)
+
+const f = fruits.findIndex(fruit => /^C/.test(fruit))
+console.log(f)
+
+// .includes()
+
+const g = numbers.includes(3)
+console.log(g)
+
+const h = fruits.includes('HI')
+console.log(h)
+
+  // .push()  .unshfit()
+// 원본 수정됨 주의!
+
+// numbers.push(5)
+// console.log(numbers)
+
+// numbers.unshift(0)
+// console.log(numbers)
+
+  // .reverse()     원본 수정
+
+// numbers.reverse()
+// fruits.reverse()
+
+// console.log(numbers)
+// console.log(fruits)
+
+  // .splice()    원본 수정
+
+numbers.splice(1, 2, 99)
+console.log(numbers)
+
+fruits.splice(2, 0 , 'orange')
+console.log(fruits)
