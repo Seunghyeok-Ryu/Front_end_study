49f389d3b528645e78711aafd1ede3bf68be3c9c
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-02-09 17:38:41 +0900

    22/02/09/04_02_02

---
+++ part4/js_study_02/main.js
@@ -0,0 +1,42 @@
+import checkType from './getType'
+// import {random, user as RSH} from './getRandom'
+import * as R from './getRandom'
+
+console.log(_.camelCase('the hello world'))
+console.log(checkType(1,2,3))
+// console.log(random(), random())
+console.log(R)
+
+// lodash 사용법
+import _ from 'lodash'
+
+// const usersA = [
+//   { userId: '1', name:'harrison'},
+//   { userId: '2', name:'Neo'}
+// ]
+// const usersB = [
+//   { userId:'1', name:'harrison'},
+//   { userId:'3', name:'Amy'}
+// ]
+// const usersC = usersA.concat(usersB)
+// console.log('cnoncat', usersC)
+// console.log('uniqBy', _.uniqBy(usersC, 'userId'))
+
+// const usersD = _.unionBy(usersA, usersB, 'userId')
+// console.log('unionBy', usersD)
+
+const users = [
+  { userId: '1', name:'A'},
+  { userId: '2', name:'B'},
+  { userId: '3', name:'C'},
+  { userId: '4', name:'D'},
+  { userId: '5', name:'E'},
+]
+
+const foundUser = _.find(users, {name:'C'})
+const foundUserIndex  = _.findIndex(users,{name:'C'})
+console.log(foundUser)
+console.log(foundUserIndex)
+
+_.remove(users, {name:'A'})
+console.log(users)
