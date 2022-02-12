3ebb2a3fed9eecc7f0bdca07f572f655ade5ebf1
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-02-12 18:33:58 +0900

    22/02/12/04_03_05

--- part4/js_study_03/main.js
+++ part4/js_study_03/main.js
@@ -1,9 +1,10 @@
 const str = `
 010-1234-5678
 thesecon@gmail.com
-http://www.omdbapi.com/?apikey=7035c60c&s=frozen
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
+http://localhost:1234
 `
 
 // const regexp = new RegExp('the','gi')
@@ -12,4 +13,8 @@ abbcccdddd
 // console.log(str.replace(regexp, 'AAA'))
 // replace 원본에 영향을 끼치지 않음
 
-console.log(str.match(/\.$/gim))
+// \. -> 특수 문자(.)앞에 문자로 보기 위해 사용
+console.log(
+  str.match(/\b\w{2,3}\b/g)
+)
+
