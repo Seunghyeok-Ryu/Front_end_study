496dc89f1608dd475f67e9f2686fc3f447e4993e
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-02-12 18:10:43 +0900

    22/02/12/04_03_03

--- part4/js_study_03/main.js
+++ part4/js_study_03/main.js
@@ -1,5 +1,5 @@
 const str = `
-010-1234-1234
+010-1234-5678
 thesecon@gmail.com
 http://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
@@ -7,5 +7,7 @@ abbcccdddd
 `
 
 // const regexp = new RegExp('the','gi')
-const regexp = /the/gi
-console.log(str.match(regexp))
+const regexp = /fox/gi
+// console.log(regexp.test(str))
+console.log(str.replace(regexp, 'AAA'))
+// replace 원본에 영향을 끼치지 않음
