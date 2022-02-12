440fcdfa1444f1c7286cdcd079e2f984458448ee
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-02-12 18:48:33 +0900

    22/02/12/04_03_06

--- part4/js_study_03/main.js
+++ part4/js_study_03/main.js
@@ -1,10 +1,9 @@
 const str = `
 010-1234-5678
 thesecon@gmail.com
-https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+http://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
-http://localhost:1234
 `
 
 // const regexp = new RegExp('the','gi')
@@ -14,7 +13,8 @@ http://localhost:1234
 // replace 원본에 영향을 끼치지 않음
 
 // \. -> 특수 문자(.)앞에 문자로 보기 위해 사용
+
 console.log(
-  str.match(/\b\w{2,3}\b/g)
+  str.match(/(?<=@).{1,}/g)
 )
 
